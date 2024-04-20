5fed55c504087ba6f77eac07554f0d2389a2d17c
ruben <rubenfarias.18c@gmail.com>
2024-04-19 21:52:59 -0600

    Optimization changes

--- leet_code/fizzbuzz.py
+++ leet_code/fizzbuzz.py
@@ -9,35 +9,25 @@
 #Input: n = 15
 #Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
 
-
-
 def FizzBuzz():
     try:
         answer=int(input("Please enter a number between 1 and 100:"))
     except:
-        print("you really don´t like folliwing rules right? Please, just a number between 1 and 100 ;)")
-
-
+        print("you really don´t like folliwing rules right? ;)")
     else:
         print("gracias!")
         if answer > 0 and answer < 100:
             number=list((range(1,answer+1)))
-
             for value in number:
                 result_3=value / 3 
                 result_5=value / 5
-                
-                if (result_3,float) or (result_5,float):
-                    if result_3 == int(result_3):
-                        number[value-1]=str()
-                        number[value-1]="Fizz"
-                    if result_5 == int(result_5):
-                        number[value-1]=str()
-                        number[value-1]="Buzz"
-                        
-                    
+                if result_3 == int(result_3):
+                    number[value-1]=str("Fizz")
+                elif result_5 == int(result_5):
+                    number[value-1]=str("Buzz")
+                else:
+                    number[value-1]=str(value)
     print(number)
-
 
 FizzBuzz()
 

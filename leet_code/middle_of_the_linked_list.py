75e6f4dbdfaf6c80237e8fb79b665b7bef77ed04
ruben <rubenfarias.18c@gmail.com>
2024-04-24 22:11:13 -0600

    Finished

--- leet_code/middle_of_the_linked_list.py
+++ leet_code/middle_of_the_linked_list.py
@@ -1,44 +1,55 @@
-
 #Given the head of a singly linked list, return the middle node of the linked list.
 #If there are two middle nodes, return the second middle node.
+#Example 1:                     #Input: head = [1,2,3,4,5]
+                                #Output: [3,4,5]
 
-#Example 1:
-#Input: head = [1,2,3,4,5]
-#Output: [3,4,5]
-#Explanation: The middle node of the list is node 3.
-
-#Example 2:
-#Input: head = [1,2,3,4,5,6]
-#Output: [4,5,6]
+#Example 2:                     #Input: head = [1,2,3,4,5,6]
+                                #Output: [4,5,6]
 #Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
-
 class node:
-    def __init__(self, data, next=None):
-        self.data=data
-        self.next=next
-
+    def __init__(self, data):                
+        self.data=data                                      
+        self.next=None
 class linked_list:
     def __init__(self):
-        self.head=None
-
+        self.head = None
+    def length(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.next
+        return count
     def insert(self, data):
-        new_node=node(data)
-        if(self.head):
-            current=self.head
-            while (current.next):
-                current=current.next
-            current.next=new_node
+        new_node = node(data)
+        if not self.head:
+            self.head = new_node
         else:
-            self.head=new_node
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+    def print_middle_of_the_linked_list(self):
+        slow_ptr = self.head
+        fast_ptr = self.head
+        if self.head is not None:
+            while(fast_ptr is not None and fast_ptr.next is not None and fast_ptr.next.next is not None):
+                fast_ptr = fast_ptr.next.next
+                slow_ptr = slow_ptr.next     
+        if self.length() % 2 == 0 and slow_ptr and slow_ptr.next:
+            slow_ptr = slow_ptr.next
+        while slow_ptr:
+            print(slow_ptr.data)
+            slow_ptr = slow_ptr.next
+    def inputt(self):
+        print("Este programa devolverá el nodo medio de una lista enlazada.")
+        while True:
+            answer = input("Por favor, ingresa un número, o, presiona f para terminar:")
+            if answer.lower() == "f":
+                break
+            else:
+                self.insert(answer)
 
-    def printLL(self):
-        current=self.head
-        while (current):
-            print(current.data)
-            current=current.next
-
-LL=linked_list()
-LL.insert(3)
-LL.insert(4)
-LL.insert(5)
-LL.printLL()
+LL = linked_list()
+LL.inputt()
+LL.print_middle_of_the_linked_list()
